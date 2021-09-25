afc947bfb8d2abdee27f828f2a9e3e30ae2dccb7
林中钓獒 <3096788874@qq.com>
2021-09-25 18:07:11 +0800

    2021/9/25/18:05

---
+++ BubbleSort.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<time.h>
+#define MyArrayNums 10
+using namespace std;
+//冒泡排序
+//大泡下沉, 小泡上浮
+//每一轮最大的元素都会排到数组的末端
+void test(vector<int>&vec, const int& height)
+{
+	//冒泡排序
+	for (int i = 0; i < height; ++i)
+	{
+		for (int j = 0; j < height - i - 1; j++)
+		{
+			if (vec[j] > vec[j + 1])
+			{
+				swap(vec[j], vec[j + 1]);
+			}
+		}
+	}
+
+	//打印排序后数组
+	for (auto i : vec)
+	{
+		cout << i << " ";
+	}
+	cout << endl;
+}
+int main()
+{
+	vector<int>vec;
+	srand((int)time(0));
+	int nums = MyArrayNums;
+
+	for (int i = 0; i < nums; i++)
+	{
+		vec.push_back(rand());
+	}
+
+	//打印排序前数组
+	for (auto i : vec)
+	{
+		cout << i << " ";
+	}
+	cout << endl;
+
+	test(vec, nums);
+
+	system("pause");
+	return 0;
+}
---
+++ SelectionSort.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<time.h>
+#define MyArrayNums 10
+using namespace std;
+//选择排序
+//每一轮循环找到最小的那个数, 排到最前面
+void test(vector<int>&vec, const int& nums)
+{
+	for (int i = 0; i < nums; ++i)
+	{
+		for (int j = i + 1; j < nums; ++j)
+		{
+			if (vec[i] > vec[j])
+			{
+				swap(vec[i], vec[j]);
+			}
+		}
+	}
+	//打印排序后数组
+	for (auto i : vec)
+	{
+		cout << i << " ";
+	}
+	cout << endl;
+}
+
+int main()
+{
+	vector<int>vec;
+	srand((int)time(0));
+	int nums = MyArrayNums;
+
+	for (int i = 0; i < nums; i++)
+	{
+		vec.push_back(rand());
+	}
+
+	//打印排序前数组
+	for (auto i : vec)
+	{
+		cout << i << " ";
+	}
+	cout << endl;
+
+	test(vec, nums);
+
+	system("pause");
+	return 0;
+}
